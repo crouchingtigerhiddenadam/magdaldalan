36fe79684d2525bd8cc5aa0d4592460d3dc43015
crouchingtigerhiddenadam <adam.d.finney@googlemail.com>
2019-08-13 14:24:51 +0100

    ajax fixed

---
+++ message/_send.php
@@ -0,0 +1,24 @@
+<?php
+
+    require_once '../config.php';
+
+    $user_id = 2;
+    
+    if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
+
+        $content = htmlentities( $_POST[ 'content' ] );
+
+        $db_connection = new mysqli( $db_server, $db_username, $db_password, $db_name );
+        $db_statement = $db_connection->prepare( 'INSERT INTO message(user_id, content, creation_datetime_utc) VALUES(?, ?, UTC_DATE());' );
+        $db_statement->bind_param( 'is', $user_id, $content );
+        $db_statement->execute();
+        $db_statement->close();
+        $db_connection->close();
+    }
+
+?>
+
+<form action="index.php" id="form" method="post" onsubmit="send( event )">
+    <textarea id="content" name="content"></textarea>
+    <button type="submit">Send</button>
+</form>
--- message/index.php
+++ message/index.php
@@ -1,59 +1,46 @@
-<?php
-
-    require_once '../config.php';
-
-    $user_id = 2;
-    
-    if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
-
-        $content = htmlentities( $_POST[ 'content' ] );
-
-        $db_connection = new mysqli( $db_server, $db_username, $db_password, $db_name );
-        $db_statement = $db_connection->prepare( 'INSERT INTO message(user_id, content, creation_datetime_utc) VALUES(?, ?, UTC_DATE());' );
-        $db_statement->bind_param( 'is', $user_id, $content );
-        $db_statement->execute();
-        $db_statement->close();
-        $db_connection->close();
-    }
-
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
+
 </head>
 <body>
-    <section id="output">
-        <?php include 'history.php' ?>
+    <section id="history">
+        <?php include '_history.php' ?>
     </section>
-    <form id="form" method="post">
-        <textarea id="content" name="content"></textarea>
-        <button type="submit">Send</button>
-    </form>
+    <section id="send">
+        <?php include '_send.php' ?>
+    </section>
     <script>
 
-        let form = document.getElementById( 'form' )
-        let output = document.getElementById( 'output' )
-        let content = document.getElementById( 'content' )
+        function send( e ) {
+            e.preventDefault()
+            try {
+                let xhr = new XMLHttpRequest()
+                xhr.onreadystatechange = function() {
+                    if ( this.readyState == 4 && this.status == 200 ) {
+                        document.getElementById( 'send' ).innerHTML = this.responseText
+                        update()
+                    }
+                }
+                xhr.open( 'POST', '_send.php', true )
+                xhr.setRequestHeader( 'Content-type', 'application/x-www-form-urlencoded' )
+                xhr.send( 'content=' + encodeURI( document.getElementById( 'content' ).value ) )
+                return false
+            }
+            catch( exception ) {
+                console.log( exception )
+            }
+        }
 
         function update() {
             let xhr = new XMLHttpRequest()
             xhr.onreadystatechange = function() {
-                if ( this.readyState == 4 && this.status == 200 ) output.innerHTML = this.responseText
+                if ( this.readyState == 4 && this.status == 200 ) {
+                    document.getElementById( 'history' ).innerHTML = this.responseText
+                }
             }
-            xhr.open( 'GET', 'history.php', true )
+            xhr.open( 'GET', '_history.php', true )
             xhr.send()
-        }
-
-        form.onsubmit = function ( e ) {
-            e.preventDefault()
-            let xhr = new XMLHttpRequest()
-            xhr.onreadystatechange = function() {
-                if ( this.readyState == 4 && this.status == 200 ) update()
-            }
-            xhr.open( 'POST', 'index.php', true )
-            xhr.setRequestHeader( 'Content-type', 'application/x-www-form-urlencoded' )
-            xhr.send( 'content=' + encodeURI( content.value ) )
         }
 
         setInterval( update, 5000 )
