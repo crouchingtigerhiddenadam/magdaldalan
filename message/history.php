5585d3f778c46efe66094a04e2985236b06bd61d
crouchingtigerhiddenadam <adam.d.finney@googlemail.com>
2019-08-13 12:47:43 +0100

    history and polling

---
+++ message/history.php
@@ -0,0 +1,22 @@
+<?php
+
+    require_once '../config.php';
+
+    $user_id = $_SESSION[ 'user_id' ];
+
+    $db_connection = new mysqli( $db_server, $db_username, $db_password, $db_name );
+    $db_statement = $db_connection->prepare( 'SELECT * FROM message WHERE user_id = ?;' );
+    $db_statement->bind_param( 'i', $user_id );
+    $db_statement->execute();
+    $db_messages = $db_statement->get_result();
+    $db_statement->close();
+
+    $db_connection->close();
+
+?>
+
+<?php foreach ( $db_messages as $db_message ) { ?>
+<p>
+    <?= $db_message[ 'content' ] ?>
+</p>
+<?php } ?>
--- message/index.php
+++ message/index.php
@@ -2,25 +2,21 @@
 
     require_once '../config.php';
 
-    $user_id = 2;
-    $content = htmlentities( $_POST[ 'content' ] );
+    $_SESSION[ 'user_id' ] = 2
 
-    $db_connection = new mysqli( $db_server, $db_username, $db_password, $db_name );
-
+    $user_id = $_SESSION[ 'user_id' ];
+    
     if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
+
+        $content = htmlentities( $_POST[ 'content' ] );
+
+        $db_connection = new mysqli( $db_server, $db_username, $db_password, $db_name );
         $db_statement = $db_connection->prepare( 'INSERT INTO message(user_id, content, creation_datetime_utc) VALUES(?, ?, UTC_DATE());' );
         $db_statement->bind_param( 'is', $user_id, $content );
         $db_statement->execute();
         $db_statement->close();
+        $db_connection->close();
     }
-
-    $db_statement = $db_connection->prepare( 'SELECT * FROM message WHERE user_id = ?;' );
-    $db_statement->bind_param( 'i', $user_id );
-    $db_statement->execute();
-    $db_messages = $db_statement->get_result();
-    $db_statement->close();
-
-    $db_connection->close();
 
 ?>
 
@@ -29,14 +25,25 @@
 <head>
 </head>
 <body>
-    <?php foreach ( $db_messages as $db_message ) { ?>
-    <p>
-        <?= $db_message[ 'content' ] ?>
-    </p>
-    <?php } ?>
+    <section id="output">
+    </section>
     <form method="post">
         <textarea name="content"></textarea>
         <button type="submit">Send</button>
     </form>
+    <script>
+
+        let output = document.getElementById( 'output' );
+
+        setInterval( function() {
+            let xhr = new XMLHttpRequest()
+            xhr.onreadystatechange = function() {
+                if ( this.readyState == 4 && this.status == 200 ) output.innerHTML = this.responseText
+            }
+            xhr.open( 'get', 'history.php', true)
+            xhr.send()
+        }, 5000 )
+
+    </script>
 </body>
 </html>
