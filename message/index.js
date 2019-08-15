7c924b7596955ce8ec246ed5f8b165442e82edb5
crouchingtigerhiddenadam <adam.d.finney@googlemail.com>
2019-08-15 13:53:18 +0100

    minor javascript updates

--- message/index.js
+++ message/index.js
@@ -1,3 +1,27 @@
+function receive() {
+    let request = new XMLHttpRequest()
+    request.onreadystatechange = function() {
+        if ( request.readyState == 4 && request.status == 200 ) {
+            let response, section
+
+            // update contacts
+            response = request.responseText.match(/<section\s+id="contacts">[\S\s]*?<\/section>/gi)[0]
+            section = document.getElementById( 'contacts' )
+            if ( section.outerHTML != response ) section.outerHTML = response
+
+            // update messages
+            response = request.responseText.match(/<section\s+id="messages">[\S\s]*?<\/section>/gi)[0]
+            section = document.getElementById( 'messages' )
+            if ( section.outerHTML !== response ) {
+                section.outerHTML = response
+                scroll()
+            }
+        }
+    }
+    request.open( 'POST', '_received.php' + location.search, true )
+    request.send()
+}
+
 function scroll() {
     let section = document.getElementById( 'messages' )
     section.scrollTop = section.scrollHeight
@@ -17,7 +41,7 @@ function send( e ) {
             // focus content textbox
             control = document.getElementById( 'content' )
             control.focus()
-            update()
+            receive()
         }
     }
     request.open( 'POST', '_send.php' + location.search, true )
@@ -26,29 +50,5 @@ function send( e ) {
     return false
 }
 
-function update() {
-    let request = new XMLHttpRequest()
-    request.onreadystatechange = function() {
-        if ( request.readyState == 4 && request.status == 200 ) {
-            let response, section
-
-            // update contacts
-            response = request.responseText.match(/<section\s+id="contacts">[\S\s]*?<\/section>/gi)[0]
-            section = document.getElementById( 'contacts' )
-            if ( section.outerHTML !=  response ) section.outerHTML = response
-
-            // update messages
-            response = request.responseText.match(/<section\s+id="messages">[\S\s]*?<\/section>/gi)[0]
-            section = document.getElementById( 'messages' )
-            if ( section.outerHTML !== response ) {
-                section.outerHTML = response
-                scroll()
-            }
-        }
-    }
-    request.open( 'POST', '_received.php' + location.search, true )
-    request.send()
-}
-
-setInterval( update, 5000 )
+setInterval( receive, 5000 )
 scroll()
